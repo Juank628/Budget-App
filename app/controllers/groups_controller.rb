ce3a4a74e2645376b5110f17cb08b282494eeeab
Juank628 <jsanchez_zunino@yahoo.com>
2023-04-26 22:31:02 -0500

    Create transactions view and logic

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,26 +1,26 @@
 class GroupsController < ApplicationController
-    def index
-        @groups = current_user.groups
-    end
-    
-    def new
-        @group = Group.new
-    end
+  def index
+    @groups = current_user.groups
+  end
 
-    def create
-        @group = current_user.groups.new(group_params)
-        @group.user = current_user
+  def new
+    @group = Group.new
+  end
 
-        respond_to do |format|
-            if @group.save
-                format.html { redirect_to root_path }
-            else
-                format.html { render :new }
-            end
-        end
-    end
+  def create
+    @group = current_user.groups.new(group_params)
+    @group.user = current_user
 
-    def group_params
-        params.require(:group).permit(:name, :icon)
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to root_path }
+      else
+        format.html { render :new }
+      end
     end
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
